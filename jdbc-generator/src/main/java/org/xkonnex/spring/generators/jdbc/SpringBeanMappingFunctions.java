5fd12cde85185a5be1165b021867c4786eb4a5d1
A1245F6 <A1245F6@KOENB12645.koeln.ads.risnet.de>
2018-03-08 22:48:07 +0100

    added underscoreNameNew function to reflect BeanPropertyRowMapper behavior in Spring 3.2.7 and later

--- jdbc-generator/src/main/java/org/xkonnex/spring/generators/jdbc/SpringBeanMappingFunctions.java
+++ jdbc-generator/src/main/java/org/xkonnex/spring/generators/jdbc/SpringBeanMappingFunctions.java
@@ -15,15 +15,20 @@
  */
 package org.xkonnex.spring.generators.jdbc;
 
+import org.springframework.util.StringUtils;
+
 public class SpringBeanMappingFunctions {
+
 	/**
 	 * Copied from Springs BeanPropertyRowMapper 
 	 * Convert a name in camelCase to an underscored name in lower case.
 	 * Any upper case letters are converted to lower case with a preceding underscore.
+	 * Used until Spring 3.2.6
+	 * 
 	 * @param name the string containing original name
 	 * @return the converted name
 	 */
-	public String underscoreName(String name) {
+	public String underscoreName(final String name) {
 		StringBuilder result = new StringBuilder();
 		if (name != null && name.length() > 0) {
 			result.append(name.substring(0, 1).toLowerCase());
@@ -32,13 +37,38 @@ public class SpringBeanMappingFunctions {
 				if (s.equals(s.toUpperCase())) {
 					result.append("_");
 					result.append(s.toLowerCase());
-				}
-				else {
+				} else {
 					result.append(s);
 				}
 			}
 		}
 		return result.toString();
 	}
-
+	
+	/**
+	 * Copied from Springs BeanPropertyRowMapper 
+	 * Convert a name in camelCase to an underscored name in lower case.
+	 * Any upper case letters are converted to lower case with a preceding underscore.
+	 * Used in Spring 3.2.7 and later
+	 * 
+	 * @param name the string containing original name
+	 * @return the converted name
+	 */
+	public String underscoreNameNew(final String name) {
+		if (!StringUtils.hasLength(name)) {
+			return "";
+		}
+		StringBuilder result = new StringBuilder();
+		result.append(name.substring(0, 1).toLowerCase());
+		for (int i = 1; i < name.length(); i++) {
+			String s = name.substring(i, i + 1);
+			String slc = s.toLowerCase();
+			if (!s.equals(slc)) {
+				result.append("_").append(slc);
+			} else {
+				result.append(s);
+			}
+		}
+		return result.toString();
+	}
 }
--- jdbc-generator/src/test/java/org/xkonnex/spring/generators/jdbc/ParameterSourceGeneratorTest.xtend
+++ jdbc-generator/src/test/java/org/xkonnex/spring/generators/jdbc/ParameterSourceGeneratorTest.xtend
@@ -1,8 +1,9 @@
 package org.xkonnex.spring.generators.jdbc
 
+import static org.junit.Assert.*;
+
 import com.google.inject.Guice
 import java.io.File
-import org.junit.Assert
 import org.junit.Test
 
 class ParameterSourceGeneratorTest {
@@ -15,6 +16,6 @@ class ParameterSourceGeneratorTest {
 		val gen = injector.getInstance(ParameterSourceGenerator)
 		gen.generate(SampleBean)
 		val genFile = new File('''src«File.separator»gen«File.separator»java«File.separator»org«File.separator»xkonnex«File.separator»spring«File.separator»generators«File.separator»jdbc«File.separator»SampleBeanParameterBuilder.java''')
-		Assert.assertTrue(genFile.exists)
+		assertTrue(genFile.exists)
 	}
 }
